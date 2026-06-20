2953c103c7ac274874887c214563008f9c1c1efc
killioiden <derek.verduijn@proton.me>
2026-06-20 15:06:09 +0200

    Added hyprland config

---
+++ hypr/programs.lua
@@ -0,0 +1,12 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local programs = {
+  terminal    = "kitty",
+  fileManager = "dolphin",
+  menu        = "fuzzel",
+}
+
+return programs
